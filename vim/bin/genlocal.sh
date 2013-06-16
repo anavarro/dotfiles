2c4692c5a5befe21163256c62a9912bcaeac9deb
Alvaro Navarro <alnacle@gmail.com>
2013-06-16 21:26:49 +0200

    Removed plugin bundles. Switching to vundle

--- vim/bin/genlocal.sh
+++ vim/bin/genlocal.sh
@@ -1,14 +1,28 @@
 #!/bin/bash 
 
-if [ -f ../vimrc_local ];
-then
-    rm -f ../vimrc_local
-fi
+cflags=`which ctags`
+
+vimcfg=../vimrc_local
+
+if [ -z $cflags ]; then
+    echo "Error: Exuberant ctags must be installed before continue"
+    exit 1
+fi 
+
+if [ -f ${vimcfg} ]; then
+    rm -f ${vimcfg}
+fi 
 
 for i in `cat *.list`;
 do
-    echo "set path+=$i/**" >> ../vimrc_local
-    echo "set tags+=`pwd`/../tags/`basename $i`" >> ../vimrc_local
-    ctags -R --sort=1 --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++ -f ../tags/`basename $i` $i
+    if [ ! -d $i ]; then
+        echo "No sources found in $i. Skipping..."
+    else
+        echo "Generating tags for $i..."
+        ctags -R --sort=1 --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++ -f ../tags/`basename $i` $i
+        echo "Adding references to vim..."
+        echo "set path+=$i/**" >> ${vimcfg}
+        echo "set tags+=`pwd`/../tags/`basename $i`" >> ${vimcfg}
+    fi
 done
 
